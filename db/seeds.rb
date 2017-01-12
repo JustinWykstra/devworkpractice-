b13e1645d3e29a01fa3a40af34cbf8f42574b7f1
JustinWykstra <justinwyks@gmail.com>
2017-01-12 10:41:44 -0700

    Implemented custom scopes for portfolio items

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
+3.times do |topic|
+  Topic.create!(
+    title: "Topic #{topic}"
+    )
+end
+
+puts "3 Topics created"
+
 10.times do |blog|
   Blog.create!(
     title: "My Blog Post #{blog}",
-    body: "Aenean ligula nulla, elementum vitae vulputate vitae, auctor a massa. Fusce dignissim euismod felis a sollicitudin. Suspendisse sagittis mollis mi, eget venenatis leo suscipit et. Vestibulum dapibus magna libero, sed porttitor felis condimentum vitae. Quisque mollis nisl tortor, et imperdiet nisl aliquam ac. Nullam posuere odio magna, ut porttitor erat consequat ut. In accumsan eros eu enim blandit, non malesuada erat tincidunt. Sed in laoreet nibh, vel volutpat neque. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aenean magna felis, faucibus id consequat et, rhoncus sit amet nisi."
+    body: "Aenean ligula nulla, elementum vitae vulputate vitae, auctor a massa. Fusce dignissim euismod felis a sollicitudin. Suspendisse sagittis mollis mi, eget venenatis leo suscipit et. Vestibulum dapibus magna libero, sed porttitor felis condimentum vitae. Quisque mollis nisl tortor, et imperdiet nisl aliquam ac. Nullam posuere odio magna, ut porttitor erat consequat ut. In accumsan eros eu enim blandit, non malesuada erat tincidunt. Sed in laoreet nibh, vel volutpat neque. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aenean magna felis, faucibus id consequat et, rhoncus sit amet nisi.",
+    topic_id: Topic.last.id
   )
 end
 
@@ -16,10 +25,20 @@ end
 
 puts "5 skills created"
 
-9.times do |portfolio_item|
+8.times do |portfolio_item|
   Portfolio.create!(
     title: "Portfolio title: #{portfolio_item}",
-    subtitle: "My great service",
+    subtitle: "Ruby on Rails",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vehicula pharetra nulla. Nam sed augue vel metus lacinia lobortis dignissim faucibus elit. Curabitur laoreet libero diam, eget faucibus enim rutrum sodales. Aenean vel interdum sem. Pellentesque efficitur sapien vel nulla aliquet varius. Donec facilisis euismod nulla in bibendum. Donec pellentesque velit ut lectus gravida semper. Phasellus ultricies facilisis dui, ac posuere augue posuere quis. Curabitur et pharetra ante, quis pharetra neque.",
+    main_image: "http://placehold.it/600x400",
+    thumb_image: "http://placehold.it/350x200"
+  )
+end
+
+1.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio title: #{portfolio_item}",
+    subtitle: "AngularTS",
     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vehicula pharetra nulla. Nam sed augue vel metus lacinia lobortis dignissim faucibus elit. Curabitur laoreet libero diam, eget faucibus enim rutrum sodales. Aenean vel interdum sem. Pellentesque efficitur sapien vel nulla aliquet varius. Donec facilisis euismod nulla in bibendum. Donec pellentesque velit ut lectus gravida semper. Phasellus ultricies facilisis dui, ac posuere augue posuere quis. Curabitur et pharetra ante, quis pharetra neque.",
     main_image: "http://placehold.it/600x400",
     thumb_image: "http://placehold.it/350x200"
